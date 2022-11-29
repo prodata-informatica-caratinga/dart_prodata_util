bf9dc285e965ac05f39dc18bdffcbe60d0c6117e
Frankley Patrício <frankleypatricio@hotmail.com>
2022-11-29 15:54:51 -0300

    - Criado export e atualizado o README

--- lib/dart_prodata_util.dart
+++ lib/dart_prodata_util.dart
@@ -4,3 +4,33 @@ export 'src/config.dart';
 
 // Componentes
 export 'src/components/currency-field.dart';
+export 'src/components/custom-bottomsheet.dart';
+export 'src/components/custom-card.dart';
+export 'src/components/custom-label.dart';
+
+// Dialogs
+export 'src/dialogs/erro-bottomsheet.dart';
+export 'src/dialogs/generic-dialogs.dart';
+export 'src/dialogs/loading-dialog.dart';
+
+// Exceptions
+export 'src/exceptions/api-error-exception.dart';
+export 'src/exceptions/common-exception.dart';
+
+// Helpers
+export 'src/helpers/formatter.dart';
+export 'src/helpers/pro-util.dart';
+export 'src/helpers/text-input-formatters.dart';
+export 'src/helpers/validator.dart';
+
+//HTTP
+export 'src/http/interceptors/logging.dart';
+export 'src/http/apiprovider.dart';
+
+// Models
+export 'src/models/api-data.dart';
+export 'src/models/app-error.dart';
+export 'src/models/util-theme.dart';
+
+// Snackbars
+export 'src/snackbars/rounded-snackbar.dart';
