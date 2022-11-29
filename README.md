c95b692cd32043efaf0a14a91cde0d1909fe8b0a
Frankley Patrício <frankleypatricio@hotmail.com>
2022-11-29 17:30:46 -0300

    - Ajuste no custom card | Atualizado README

--- README.md
+++ README.md
@@ -9,6 +9,41 @@ Adicione a linha abaixo no pubspec.yaml.
 ```yaml
 dart_prodata_util:
   git:
-    url: git@github.com:prodata-informatica-caratinga/dart-prodata-util.git
+    url: https://github.com/prodata-informatica-caratinga/dart_prodata_util.git
     ref: main
+```
+
+Depois adicione a navigatorKey no seu MaterialApp.
+
+```dart
+import 'package:dart_prodata_util/dart_prodata_util.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      key: Config.navigatorKey, // Adicione essa linha
+    );
+  }
+}
+```
+
+Por fim sobrescrevaa função didChangeDependencies na sua Splash Screen e chame a função initConfig.
+
+``` dart
+@override
+void didChangeDependencies() {
+  Config.initConfig(context);
+  super.didChangeDependencies();
+}
 ```
--- lib/src/components/custom-card.dart
+++ lib/src/components/custom-card.dart
@@ -1,36 +1,48 @@
 import 'package:dart_prodata_util/src/config.dart';
 import 'package:flutter/material.dart';
 
-Card customCard({
-  required String title,
-  String? description,
-  required Widget child,
-  EdgeInsets padding = const EdgeInsets.only(left: 8, right: 8, top: 16),
-  double elevation = 0.0,
-}) {
-  assert(Config.navigatorKey.currentContext != null, 'Context não encontrado. Verifique se a navigatorKey está definida no MaterialApp.');
-  return Card(
-    elevation: elevation,
-    margin: const EdgeInsets.only(left: 3, right: 3, top: 3, bottom: 3),
-    child: Padding(
-      padding: padding,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(title, style: Config.utilTheme.customCardTheme.titleStyle),
-          if (description != null)
-            Text(
-              description,
-              style: Config.utilTheme.customCardTheme.descriptionStyle,
-              textAlign: TextAlign.justify,
+class CustomCard extends StatelessWidget {
+  final String title;
+  final String? description;
+  final Widget child;
+  final EdgeInsets padding;
+  final double elevation;
+
+  CustomCard({
+    required this.title,
+    this.description,
+    required this.child,
+    this.padding = const EdgeInsets.only(left: 8, right: 8, top: 16),
+    this.elevation = 0.0,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    assert(Config.navigatorKey.currentContext != null, 'Context não encontrado. Verifique se a navigatorKey está definida no MaterialApp.');
+    return Card(
+      elevation: elevation,
+      margin: const EdgeInsets.only(left: 3, right: 3, top: 3, bottom: 3),
+      child: Padding(
+        padding: padding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title, style: Config.utilTheme.customCardTheme.titleStyle),
+            if (description != null)
+              Text(
+                description!,
+                style: Config.utilTheme.customCardTheme.descriptionStyle,
+                textAlign: TextAlign.justify,
+              ),
+            Divider(color: Theme.of(Config.navigatorKey.currentContext!).colorScheme.primary.withOpacity(.6)),
+            Padding(
+              padding: const EdgeInsets.only(top: 0, bottom: 8, left: 12, right: 8),
+              child: child,
             ),
-          Divider(color: Theme.of(Config.navigatorKey.currentContext!).colorScheme.primary.withOpacity(.6)),
-          Padding(
-            padding: const EdgeInsets.only(top: 0, bottom: 8, left: 12, right: 8),
-            child: child,
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
